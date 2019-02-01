0726033bfbe916dc6c73547e74c878fd5cc504bc
nenoNaninu <neno@neno-garden.com>
2019-02-01 18:22:07 +0900

    add: 擬似逆行列を使って4点以上に対応

--- HomographySharp/HomographyHelper.cs
+++ HomographySharp/HomographyHelper.cs
@@ -16,9 +16,9 @@ namespace HomographySharp
         /// <returns>Homography Matrix</returns>
         public static DenseMatrix FindHomography(List<DenseVector> srcPoints, List<DenseVector> dstPoints)
         {
-            if (srcPoints.Count != 4 || dstPoints.Count != 4)
+            if (srcPoints.Count != dstPoints.Count)
             {
-                throw new ArgumentException("srcPoints and dstPoints need just 4 points");
+                throw new ArgumentException("srcPoints and dstPoints must same num");
             }
 
             if (srcPoints.Any(x => x.Count != 2) || dstPoints.Any(x => x.Count != 2))
@@ -29,9 +29,10 @@ namespace HomographySharp
             //q(dstのベクトル) = A(作成するべき8x8行列) * P(射影変換のパラメータ)
             //P = A^-1 * q
             //でパラメータが求まる。
-            DenseMatrix a = DenseMatrix.Create(8, 8, 0);
+            int pointNum = srcPoints.Count;
+            DenseMatrix a = DenseMatrix.Create(pointNum * 2, 8, 0);
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < pointNum; i++)
             {
                 var src = srcPoints[i];
                 var dst = dstPoints[i];
@@ -45,17 +46,19 @@ namespace HomographySharp
                 var row1 = DenseVector.OfArray(new double[] { srcX, srcY, 1, 0, 0, 0, -dstX * srcX, -dstX * srcY });
                 var row2 = DenseVector.OfArray(new double[] { 0, 0, 0, srcX, srcY, 1, -dstY * srcX, -dstY * srcY });
 
-                a.SetRow(i, row1);
-                a.SetRow(i + 4, row2);
+                a.SetRow(2 * i, row1);
+                a.SetRow(2 * i + 1, row2);
             }
 
-            var inversed = a.Inverse();
+            var inversed = a.PseudoInverse();
 
-            var dstVec = DenseVector.OfArray(new double[]
+            var dstVec = DenseVector.Create(pointNum * 2, 0);
+
+            for(int i = 0; i < pointNum; i++)
             {
-                dstPoints[0][0],dstPoints[1][0],dstPoints[2][0],dstPoints[3][0],
-                dstPoints[0][1],dstPoints[1][1],dstPoints[2][1],dstPoints[3][1]
-            });
+                dstVec[i * 2] = dstPoints[i][0];
+                dstVec[i * 2 + 1] = dstPoints[i][1];
+            }
 
             var parameterVec = inversed * dstVec;
 
@@ -83,9 +86,10 @@ namespace HomographySharp
             //q(dstのベクトル) = A(作成するべき8x8行列) * P(射影変換のパラメータ)
             //P = A^-1 * q
             //でパラメータが求まる。
-            DenseMatrix a = DenseMatrix.Create(8, 8, 0);
+            int pointNum = srcPoints.Count;
+            DenseMatrix a = DenseMatrix.Create(pointNum * 2, 8, 0);
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < pointNum; i++)
             {
                 var src = srcPoints[i];
                 var dst = dstPoints[i];
@@ -93,17 +97,19 @@ namespace HomographySharp
                 var row1 = DenseVector.OfArray(new double[] { src.X, src.Y, 1, 0, 0, 0, -dst.X * src.X, -dst.X * src.Y });
                 var row2 = DenseVector.OfArray(new double[] { 0, 0, 0, src.X, src.Y, 1, -dst.Y * src.X, -dst.Y * src.Y });
 
-                a.SetRow(i, row1);
-                a.SetRow(i + 4, row2);
+                a.SetRow(i*2, row1);
+                a.SetRow(i *2 + 1, row2);
             }
 
-            var inversed = a.Inverse();
+            var inversed = a.PseudoInverse();
 
-            var dstVec = DenseVector.OfArray(new double[]
+            var dstVec = DenseVector.Create(pointNum * 2, 0);
+
+            for (int i = 0; i < pointNum; i++)
             {
-                dstPoints[0].X,dstPoints[1].X,dstPoints[2].X,dstPoints[3].X,
-                dstPoints[0].Y,dstPoints[1].Y,dstPoints[2].Y,dstPoints[3].Y
-            });
+                dstVec[i * 2] = dstPoints[i].X;
+                dstVec[i * 2 + 1] = dstPoints[i].Y;
+            }
 
             var parameterVec = inversed * dstVec;
 
