0e8d56bf6d27497d6806882298c1599b77ca8f4a
nenoNaninu <neno@neno.dev>
2019-06-17 00:53:49 +0900

    udpate 😁�

--- HomographySharp/HomographyHelper.cs
+++ HomographySharp/HomographyHelper.cs
@@ -190,16 +190,17 @@ namespace HomographySharp
             //var dst = homography * vec;
             //return (dst[0] / dst[2], dst[1] / dst[2]);
 
-            var rawArray = homography.Values;
-
-            if (rawArray.Length != 9)
+            if (homography.RowCount != 3 && homography.ColumnCount != 3)
             {
                 throw new ArgumentException("The shape of homography matrix must be 3x3");
             }
 
+            var rawArray = homography.Values;
+
             var dst1 = rawArray[0] * srcX + rawArray[3] * srcY + rawArray[6];
             var dst2 = rawArray[1] * srcX + rawArray[4] * srcY + rawArray[7];
             var dst3 = rawArray[2] * srcX + rawArray[5] * srcY + rawArray[8];
+
             return (dst1 / dst3, dst2 / dst3);
         }
     }
