0065fa507ed6d1d32c4ef4a33fc729c6e0354dc3
nenoNaninu <neno@melonpan.pro>
2021-05-20 22:34:47 +0900

    add test

--- HomographySharpTest/UnitTest1.cs
+++ HomographySharpTest/UnitTest1.cs
@@ -256,5 +256,30 @@ namespace Tests
                 Assert.IsTrue(Math.Abs(homo.Elements[i] - homo2.Elements[i]) < 0.001);
             }
         }
+
+        [Test]
+        public void Copy3()
+        {
+            var srcList = new List<Point2<double>>(4);
+            var dstList = new List<Point2<double>>(4);
+
+            srcList.Add(new Point2<double>(-152, 394));
+            srcList.Add(new Point2<double>(218, 521));
+            srcList.Add(new Point2<double>(223, -331));
+            srcList.Add(new Point2<double>(-163, -219));
+            dstList.Add(new Point2<double>(-666, 431));
+            dstList.Add(new Point2<double>(500, 300));
+            dstList.Add(new Point2<double>(480, -308));
+            dstList.Add(new Point2<double>(-580, -280));
+
+            var homo = Homography.Find(srcList, dstList);
+
+            var homo2 = Homography.Create(homo.Elements);
+
+            for (int i = 0; i < homo.Elements.Count; i++)
+            {
+                Assert.IsTrue(Math.Abs(homo.Elements[i] - homo2.Elements[i]) < 0.001);
+            }
+        }
     }
 }
